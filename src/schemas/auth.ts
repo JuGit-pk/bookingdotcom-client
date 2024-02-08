4d1add289f2f262668fb2744a690ab1d91f7c41b
Muhammad Junaid Iqbal <junaidi2k1@gmail.com>
2024-02-08 12:37:14 +0500

    - refactor(schema): confirm password error message

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,26 +1,37 @@
 import { z } from "zod";
 
-export const registerSchema = z.object({
-  firstName: z.string().min(2, { message: "at least 2 characters" }),
-  lastName: z.string().min(2, { message: "at least 2 characters" }),
-  email: z
-    .string()
-    .nonempty("Email is required")
-    .email("Please enter a valid email address."),
-  password: z
-    .string()
-    .nonempty("Password is required")
-    .min(8, "Password must be at least 8 characters"),
-  // .refine(
-  //   (value) =>
-  //     /^(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[a-zA-Z]).{8,}$/.test(value),
-  //   {
-  //     message: "Must contain at least 1 number and 1 special character",
-  //   },
-  // ),
-  // companyName: z.string().nonempty("Company name is required"),
-  // country: z.string().nonempty("Country is required"),
-});
+export const registerSchema = z
+  .object({
+    firstName: z.string().min(2, { message: "at least 2 characters" }),
+    lastName: z.string().min(2, { message: "at least 2 characters" }),
+    email: z
+      .string()
+      .nonempty("Email is required")
+      .email("Please enter a valid email address."),
+    password: z
+      .string()
+      .nonempty("Password is required")
+      .min(8, "Password must be at least 8 characters"),
+    confirmPassword: z.string(),
+    // .refine(
+    //   (value) =>
+    //     /^(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[a-zA-Z]).{8,}$/.test(value),
+    //   {
+    //     message: "Must contain at least 1 number and 1 special character",
+    //   },
+    // ),
+    // companyName: z.string().nonempty("Company name is required"),
+    // country: z.string().nonempty("Country is required"),
+  })
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        path: ["confirmPassword"],
+        message: "The passwords did not match",
+      });
+    }
+  });
 
 export const loginSchema = z.object({
   email: z
